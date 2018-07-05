500b3ec656b2faff12992117fe7caa8e2281713f
skconan <supakit.kr@gmail.com>
2018-07-05 12:20:04 +0800

    before connect with ros

--- constant.py
+++ constant.py
@@ -13,11 +13,13 @@ import math
     DICE_SIZE must be divided by 3 
 '''
 
+IMG_WIDTH = 1152
+IMG_HEIGHT = 874
 DICE_SIZE = 90
 SIDE_PER_RADIUS = 9.0
 POINT_RADIUS = float(DICE_SIZE) / SIDE_PER_RADIUS
-POINT_AREA_LOWER = math.pi * (POINT_RADIUS-1.5)**2
-POINT_AREA_UPPER = math.pi * (POINT_RADIUS+5)**2
+POINT_AREA_LOWER = math.pi * ((POINT_RADIUS-1.5)**2)
+POINT_AREA_UPPER = math.pi * ((min(IMG_WIDTH, IMG_HEIGHT) / SIDE_PER_RADIUS)**2) 
 ABS_PATH = 'C:/Users/skconan/Desktop/Workspace/dice/'
 IMG_PATH = ABS_PATH + 'images/'
 DATA_SET_PATH = IMG_PATH + 'data_set/'
